17c1501f0a161b7c1f5c1c687376398731f1035d
shirleytwl <shirtanwl@gmail.com>
2019-06-28 17:07:48 +0800

    first commit

--- js/google_shopping_functions.js
+++ js/google_shopping_functions.js
@@ -11,7 +11,44 @@ function getItemsCount(itemData) {
 /*
  * Define and use your functions here
  */
+function getItems(objectData) {
+    return objectData.items;
+}
+
+function getItemsByBrands(items, brand){
+    var selectedItems = [];
+    for (var i=0; i<items.length; i++){
+
+        if (items[i].product.brand === brand){
+            selectedItems.push(items[i]);
+        }
+    }
+    return selectedItems;
+}
+
+function getItemsByAuthor(items, author){
+    var selectedItems = [];
+    for (var i=0; i<items.length; i++){
+
+        if (items[i].product.author.name === author){
+            selectedItems.push(items[i].product.title);
+        }
+    }
+    return selectedItems;
+}
+
+function getAvailableProducts(items){
+    var selectedItems = [];
+    for (var i=0; i<items.length; i++){
+        console.log(items[i].product.inventories);
+        if (items[i].product.inventories[0].availability === "inStock"){
+            selectedItems.push(items[i].product.title);
+        }
+    }
+    return selectedItems;
+}
+
 
 // output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+//console.log('Item Count: ' + getItemsCount(data));
 
